c83b861212ec9c8ce0fba648ea4b1967b02708f3
KornFlex59 <65032155+KornFlex59@users.noreply.github.com>
2025-02-15 10:00:53 +0100

    tt

--- build.js
+++ build.js
@@ -66,7 +66,7 @@ class Index {
                 appId: preductname,
                 productName: preductname,
                 copyright: 'Copyright © 2020-2024 Luuxis',
-                artifactName: "${productName}-${os}.${ext}",
+                artifactName: "${productName}.${ext}",
                 extraMetadata: { main: 'app/app.js' },
                 files: ["app/**/*", "package.json", "LICENSE.md"],
                 directories: { "output": "dist" },
